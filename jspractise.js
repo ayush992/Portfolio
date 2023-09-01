d5a1fc174eef01c0da829109c482eccb02ddfb63
ayush992 <ayushm0201@gmail.com>
2023-09-01 21:21:25 +0530

    removing useless file

--- jspractise.js
+++
@@ -1,9 +0,0 @@
-for (let i = 1; i <= 5; i++) {
-    setTimeout(() =>console.log(i),1000);
-}
-console.log('---------------------');
-for (var i = 1; i <= 5; i++) {
-    setTimeout(() =>console.log(i),1000);
-}
-
-console.log('value of i ' + i);
